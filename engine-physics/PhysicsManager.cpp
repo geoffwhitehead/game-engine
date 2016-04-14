4deb83edb48c0437e8a5c0779019050793119927
Geoff Whitehead <g.whitehead@ncl.ac.uk>
2016-04-14 19:44:59 +0100

    deleted physics- working on box 2d

--- engine-physics/PhysicsManager.cpp
+++ engine-physics/PhysicsManager.cpp
@@ -2,9 +2,31 @@
 
 
 
-PhysicsManager::PhysicsManager() {
+PhysicsManager::PhysicsManager(b2World* b2_world, float b2_gravity) {
+	createWorld(b2_world, b2_gravity);
 }
 
+void PhysicsManager::createWorld(b2World* b2_world, float b2_gravity) {
+	this->b2_world = new b2World(b2Vec2(0.0f, b2_gravity));
+	this->b2_gravity = b2_gravity;
+	b2_world->SetAllowSleeping(true);
+	b2_world->SetContinuousPhysics(true);
+	//b2_world->SetContactListener(this);
+}
 
 PhysicsManager::~PhysicsManager() {
 }
+
+void update(float) {
+
+}
+void destroy() {
+
+}
+void init() {
+
+}
+void addSubSystem(SubSystem*) {
+
+}
+
--- engine-physics/PhysicsManager.h
+++ engine-physics/PhysicsManager.h
@@ -1,9 +1,21 @@
 #pragma once
 #include "../engine-base/SystemManager.h"
-class PhysicsManager :
+#include <Box2D/Box2D.h>
+
+static class PhysicsManager :
 	public SystemManager {
 public:
-	PhysicsManager();
+	PhysicsManager(b2World* world, float b2_gravity);
 	~PhysicsManager();
+
+	void update(float) = 0;
+	void destroy() = 0;
+	void init() = 0;
+	void addSubSystem(SubSystem*) = 0;
+	void createWorld(b2World* b2_world, float b2_gravity);
+
+private:
+	b2World* b2_world;
+	float b2_gravity;
 };
 
